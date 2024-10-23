f0b791371c2d1c9ec3aa92b69b99de7005255e29
viettranni <vttranviett@gmail.com>
2024-10-24 01:07:43 +0300

    Build went blank so adjusting some files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server: {
     host: true, // This will allow Vite to listen on all addresses
     port: process.env.PORT || 5173, 
+    plugins: [react()],
+    minify: 'terser', // or 'esbuild'
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
   },
 });
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
