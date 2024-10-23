bb1de7a844e422d8e68643ddac849bd37d7ccc1d
viettranni <vttranviett@gmail.com>
2024-10-24 01:00:05 +0300

    Configured the ports for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  server: {
+    host: true, // This will allow Vite to listen on all addresses
+    port: process.env.PORT || 5173, 
+  },
+});
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
